63f8de744c428c2740905d3b5dff475b7e806848
Jack <79611225@qq.com>
2015-02-28 17:29:24 +0800

    test

---
+++ Runtime/Cache/f12c0a77906a0f355c95666536d66c0f.php
@@ -0,0 +1,2 @@
+<?php if (!defined('THINK_PATH')) exit();?><html><head><title>hello <?php echo ($name); ?></title></head><body>   hello, <?php echo ($name); ?>! 
+   </body></html>
---
+++ index.php
@@ -0,0 +1,7 @@
+    <?php
+        //定义项目名称
+        define('APP_NAME', '');
+        //定义项目路径
+        define('APP_PATH', './');
+        //加载框架入文件
+        require './ThinkPHP/ThinkPHP.php';
